44d624c796fc92680380defc04553146347699b6
Banjo Donila <banjo@jumphost.lab.integrate.zone>
2021-09-23 22:26:28 +1200

    update

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.listen(8080, function() {
 })
 
 app.get('/', (req, res) => {
-  res.sendFile('./index.html')
+  res.sendFile(__dirname + 'index.html')
   // Note: __dirname is the current directory you're in. Try logging it and see what you get!
   // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
 })
